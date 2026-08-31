53aa73d59567393a0868c738b46162120d2580d1
Pip <minijag@gmail.com>
2026-08-30 19:48:26 -0700

    Add src/entry-server.jsx

---
+++ src/entry-server.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { renderToStaticMarkup } from 'react-dom/server'
+import { App } from './main.jsx'
+
+export function render() {
+  return renderToStaticMarkup(<App />)
+}
+
